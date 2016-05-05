fb957e58455994713af61149d0bed08be94b12d7
Paulo Amaral <paulo.amaral@gmail.com>
2016-05-05 22:24:33 +0000

    default behavior key-down-key-up (no repeat) when sensor is activated.

--- gpio-to-keyboard.py
+++ gpio-to-keyboard.py
@@ -1,27 +1,52 @@
+"""
+gpio-to-keyboard: Convert Raspberry GPIO state changes into keyboard events.
+
+TODO:
+  * --hold option. When on, ignores RELEASE and bind
+    sensor on/off to events key down/up respectively.
+  * --board option for GPIO.BOARD support
+  * --interval option to change the loop interval
+  * --lazy-delay --lazy-interval: after LAZY-DELAY time of inactivity,
+    enters in lazy mode, looping every LAZY-INTERVAL only to reduce
+    CPU activity
+  * --config option to support input file with list of multiple bindings
+  * create a script from this one called gpio-run to automate scripts
+    instead of emulating keyboard.
+
+ To install dependencies:
+   pip install pyautogui
+   pip install argparse
+
+Author: Paulo Amaral
+"""
+
 import RPi.GPIO as GPIO
 import time
 import pyautogui
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="Convert Raspberry GPIO state changes into keyboard events.\nrequires: pyautogui, argparse, run in a X session, root (use sudo)")
 parser.add_argument('-i','--gpio', help='GPIO port (BCM)',required=True)
-parser.add_argument('-t','--trigger', help='Key to press',required=True)
+parser.add_argument('-t','--trigger', help='Key to press when sensor activates (e.g. A B 1 2 space tab enter)',required=False)
+parser.add_argument('-r','--release', help='Key to press when sensor deactivates (e.g. A B 1 2 space tab enter)',required=False)
+parser.add_argument('-v','--verbose', help='Show debug messages', required=False, action='store_true')
 args = parser.parse_args()
 
-# configuration
-singleclick = 1 # when sensor is activated, only press key once instead
-debug = 1 # debug messages
+# debug messages
+debug = args.verbose
 
 GPIO.setmode(GPIO.BCM)
-if debug == 1: print("mode: GPIO.BCM")
+if debug: print("mode: GPIO.BCM")
 
 channel = int(args.gpio)
-if debug == 1: print("channel: gpio{0:02d}".format(channel))
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+if debug: print("channel: gpio_{0:02d} (input)".format(channel))
 
 trigger = args.trigger
+if debug: print("trigger: {0}".format(trigger))
 
-GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-if debug == 1: print("gpio{0:02d} set as GPIO.IN.".format(channel))
+release = args.release
+if debug: print("release: {0}".format(release))
 
 # keystate 0 = up, 1 = down
 keystate = 0
@@ -32,9 +57,13 @@ while True:
   if GPIO.input(channel):
     if keystate == 0:
       keystate = 1
-      pyautogui.press(trigger)
+      if trigger is not None:
+        pyautogui.press(trigger)
   else:
-    keystate = 0
+    if keystate == 1:
+      keystate = 0
+      if release is not None:
+        pyautogui.press(release)
 
     time.sleep(0.1)
 
