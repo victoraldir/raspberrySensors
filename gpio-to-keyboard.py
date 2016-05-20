dcfe56003e733d82fd62b40382eec4c5f20b14b4
Paulo Amaral <paulo.amaral@gmail.com>
2016-05-20 21:09:49 +0000

    renamed parameters "trigger" and "release" to "enter" and "leave". Added parameter for inverse logic (enter = 0, leave = 1).

--- gpio-to-keyboard.py
+++ gpio-to-keyboard.py
@@ -27,9 +27,10 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Convert Raspberry GPIO state changes into keyboard events.\nrequires: pyautogui, argparse, run in a X session, root (use sudo)")
 parser.add_argument('-i','--gpio', help='GPIO port (BCM)',required=True)
-parser.add_argument('-t','--trigger', help='Key to press when sensor activates (e.g. A B 1 2 space tab enter)',required=False)
-parser.add_argument('-r','--release', help='Key to press when sensor deactivates (e.g. A B 1 2 space tab enter)',required=False)
+parser.add_argument('-e','--enter', help='Key to press when sensor activates (e.g. A B 1 2 space tab enter)',required=False)
+parser.add_argument('-l','--leave', help='Key to press when sensor deactivates (e.g. A B 1 2 space tab enter)',required=False)
 parser.add_argument('-v','--verbose', help='Show debug messages', required=False, action='store_true')
+parser.add_argument('-0','--inverse', help='For sensors where enter=0V (low) and leave=3V (high)',required=False, action='store_true')
 args = parser.parse_args()
 
 # debug messages
@@ -42,28 +43,33 @@ channel = int(args.gpio)
 GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 if debug: print("channel: gpio_{0:02d} (input)".format(channel))
 
-trigger = args.trigger
-if debug: print("trigger: {0}".format(trigger))
+enter = args.enter
+if debug: print("enter: {0}".format(enter))
 
-release = args.release
-if debug: print("release: {0}".format(release))
+leave = args.leave
+if debug: print("leave: {0}".format(leave))
 
 # keystate 0 = up, 1 = down
 keystate = 0
 
+if (args.inverse):
+  sensor_out = True
+  sensor_in = False
+else:
+  sensor_out = False
+  sensor_in = True
+
 while True:
   time.sleep(0.00001)
 
-  if GPIO.input(channel):
+  if GPIO.input(channel) == sensor_in:
     if keystate == 0:
-      keystate = 1
-      if trigger is not None:
-        pyautogui.press(trigger)
+      keystate = 1 
+      if enter is not None: pyautogui.press(enter)
   else:
     if keystate == 1:
       keystate = 0
-      if release is not None:
-        pyautogui.press(release)
+      if leave is not None: pyautogui.press(leave)
 
     time.sleep(0.1)
 
